da3826d535d370dbf5845b99f11d62cdc914a3d0
Ana Katrina De Leon <ana.katrina.de.leon@oocl.com>
2016-06-15 18:35:25 +0800

    Home work (number 1 & 2)

---
+++ Ana Katrina De Leon/W1D3 Home work 1-2/Number 1/src/W1D3.java
@@ -0,0 +1,10 @@
+
+public class W1D3 {
+	public static void main (String[] args){
+		String name = "Kat";
+		int age = 20;
+		System.out.println("Hello!");
+		System.out.println("My name is " + name + " and my age is " + age);
+	}
+
+}
