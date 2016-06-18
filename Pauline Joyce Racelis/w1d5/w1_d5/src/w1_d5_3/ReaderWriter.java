b6be1103b56ed7fcc9744cf479e6f3525292c3f6
racelpa <pauline.joyce.racelis@oocl.com>
2016-06-18 12:59:20 +0800

    Updated HW

--- Pauline Joyce Racelis/w1d5/w1_d5/src/w1_d5_3/ReaderWriter.java
+++ Pauline Joyce Racelis/w1d5/w1_d5/src/w1_d5_3/ReaderWriter.java
@@ -18,7 +18,6 @@ public class ReaderWriter {
         
         byte byteData[] = md.digest();
 
-        //convert the byte to hex format method 1
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < byteData.length; i++) {
          sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
