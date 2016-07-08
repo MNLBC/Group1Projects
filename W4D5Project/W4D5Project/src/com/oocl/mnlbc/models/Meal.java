4978fbbc0392f73105f434e88233612277a7e04f
Aljun Dequilla <aljun.dequilla@gmail.com>
2016-07-08 09:33:59 +0800

    Updated Meal.java added date_created and date_updated

--- W4D5Project/W4D5Project/src/com/oocl/mnlbc/models/Meal.java
+++ W4D5Project/W4D5Project/src/com/oocl/mnlbc/models/Meal.java
@@ -1,5 +1,7 @@
 package com.oocl.mnlbc.models;
 
+import com.sun.jmx.snmp.Timestamp;
+
 /**
  * 
  * @author Group 1
@@ -13,61 +15,83 @@ public class Meal {
 	private String category;
 	private double price;
 	private String image;
+	private Timestamp date_created;
+	private Timestamp date_updated;
 
-	public Meal(String name, String desc, String category, double price, String image) {
+	public Meal(int id, String name, String desc, String category, double price, String image, Timestamp date_created,
+			Timestamp date_updated) {
+		this.id = id;
 		this.name = name;
 		this.desc = desc;
 		this.category = category;
 		this.price = price;
 		this.image = image;
+		this.date_created = date_created;
+		this.date_updated = date_updated;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getDesc() {
 		return desc;
 	}
 
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-
 	public String getCategory() {
 		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
 	}
 
 	public double getPrice() {
 		return price;
 	}
 
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
 	public String getImage() {
 		return image;
 	}
 
+	public Timestamp getDate_created() {
+		return date_created;
+	}
+
+	public Timestamp getDate_updated() {
+		return date_updated;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	public void setImage(String image) {
 		this.image = image;
+	}
+
+	public void setDate_created(Timestamp date_created) {
+		this.date_created = date_created;
+	}
+
+	public void setDate_updated(Timestamp date_updated) {
+		this.date_updated = date_updated;
 	}
 
 }
