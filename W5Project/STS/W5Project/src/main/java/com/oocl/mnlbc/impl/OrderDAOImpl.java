31dea0c815af031e40ee2ec2500d8109c0e6099f
Aljun Dequilla <aljun.dequilla@gmail.com>
2016-07-17 18:28:57 +0800

    fix getAllOrderByUserID

--- W5Project/STS/W5Project/src/main/java/com/oocl/mnlbc/impl/OrderDAOImpl.java
+++ W5Project/STS/W5Project/src/main/java/com/oocl/mnlbc/impl/OrderDAOImpl.java
@@ -6,6 +6,9 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.oocl.mnlbc.dao.OrderDAO;
 import com.oocl.mnlbc.dao.OrderItemsDAO;
@@ -25,6 +28,10 @@ public class OrderDAOImpl implements OrderDAO {
 	}
 
 	public DataSource getDataSource() {
+		return dataSource;
+	}
+	
+	public DataSource executeTestSource() {
 		return dataSource;
 	}
 
@@ -48,7 +55,7 @@ public class OrderDAOImpl implements OrderDAO {
 
 	@Override
 	public List<Order> getAllOrderByUserID(int id) {
-		String query = "SELECT * FROM ORDERITEMS WHERE USERS_ID = " + id;
+		String query = "SELECT * FROM ORDERS WHERE USERS_ID = " + id;
 		List<Order> orders = jdbcTemplateObject.query(query, new OrderMapper());
 		for (Order order : orders) {
 			OrderItemsDAO orderItemsDAO = new OrderItemsDAOImpl();
@@ -66,9 +73,32 @@ public class OrderDAOImpl implements OrderDAO {
 	}
 
 	@Override
-	public Order addOrderItem(Order order) {
-		// TODO Auto-generated method stub
-		return null;
+	public Order addOrder(Order order) {
+//		TransactionTemplate tt = new TransactionTemplate(getTransactionManager());
+//		tt.execute(new TransactionCallback() {
+//			public Object doInTransaction(TransactionStatus status) {
+//				JdbcTemplate jt = new JdbcTemplate(executeTestSource());
+//				StringBuilder query = new StringBuilder();
+//				query.append(
+//						"INSERT INTO USERS (USERNAME,PASSWORD,FIRSTNAME,MIDDLENAME,LASTNAME,GENDER,EMAIL,ADDRESS,CONTACTS,IS_DISABLED,TYPE) VALUES (");
+//				query.append("'" + user.getUsername() + "',");
+//				query.append("'" + user.getPassword() + "',");
+//				query.append("'" + user.getFirstname() + "',");
+//				query.append("'" + user.getMiddlename() + "',");
+//				query.append("'" + user.getLastname() + "',");
+//				query.append("'" + user.getGender() + "',");
+//				query.append("'" + user.getEmail() + "',");
+//				query.append("'" + user.getAddress() + "',");
+//				query.append("'" + user.getContact() + "',");
+//				query.append(user.isDisabled() ? "1" : "0" + ",");
+//				query.append("'" + user.getType() + "')");
+//				jt.update(query.toString());
+//
+//				return null;
+//			}
+//		});
+//
+//		return user;
 	}
 
 }
