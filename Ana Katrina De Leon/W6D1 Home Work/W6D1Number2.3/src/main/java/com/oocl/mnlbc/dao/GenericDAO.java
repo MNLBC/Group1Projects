9b9ea9fc6f43ee72b68a7634321735211d720113
Ana Katrina De Leon <ana.katrina.de.leon@oocl.com>
2016-07-19 07:11:18 +0800

    Included W6D1 number 3

---
+++ Ana Katrina De Leon/W6D1 Home Work/W6D1Number2.3/src/main/java/com/oocl/mnlbc/dao/GenericDAO.java
@@ -0,0 +1,14 @@
+package com.oocl.mnlbc.dao;
+
+import java.io.Serializable;
+
+public interface GenericDAO<T, PK extends Serializable> {
+
+	T create(T t);
+
+	T read(PK id);
+
+	T update(T t);
+
+	void delete(T t);
+}
