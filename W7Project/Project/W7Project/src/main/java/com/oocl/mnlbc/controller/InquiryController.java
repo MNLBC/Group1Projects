5e5659e9f51e5e1fcddf964af870a93e399ef9ba
Aljun Dequilla <aljun.dequilla@gmail.com>
2016-07-28 19:21:07 +0800

    Added a catch for User has already logged in
    Replace proper hashing in addUser
    Remove Unused import in InquiryController
    
    DEQUILLA

--- W7Project/Project/W7Project/src/main/java/com/oocl/mnlbc/controller/InquiryController.java
+++ W7Project/Project/W7Project/src/main/java/com/oocl/mnlbc/controller/InquiryController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.oocl.mnlbc.entity.Feedback;
 import com.oocl.mnlbc.entity.Inquiry;
 import com.oocl.mnlbc.service.impl.InquiryService;
 
