6bcc4a0452e94527e22e795a724c5e7bd56f814e
Aljun Dequilla <aljun.dequilla@gmail.com>
2016-07-29 01:24:41 +0800

    Added service and controller for TraySession
    DEQUILLA

--- W7Project/Project/W7Project/src/main/java/com/oocl/mnlbc/controller/TrayController.java
+++ W7Project/Project/W7Project/src/main/java/com/oocl/mnlbc/controller/TrayController.java
@@ -4,12 +4,16 @@
 package com.oocl.mnlbc.controller;
 
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.oocl.mnlbc.entity.Tray;
 import com.oocl.mnlbc.service.TrayService;
 
@@ -40,6 +44,18 @@ public class TrayController {
 	@RequestMapping(value = { "/addTray" }, method = RequestMethod.POST)
 	public boolean addTray(@RequestParam(required = true) String trays) {
 		return trayService.addTray(trays);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = { "/setTraySession" }, method = RequestMethod.POST)
+	public String setTraySession(@RequestParam(required = true) String trays, HttpSession session) {
+		return trayService.setTraySession(trays, session);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = { "/getTraySession" }, method = RequestMethod.POST)
+	public String getTraySession(@RequestParam(required = true) String trays, HttpSession session) {
+		return trayService.getTraySession(session);
 	}
 
 }
--- W7Project/Project/W7Project/src/main/java/com/oocl/mnlbc/service/TrayService.java
+++ W7Project/Project/W7Project/src/main/java/com/oocl/mnlbc/service/TrayService.java
@@ -2,6 +2,9 @@ package com.oocl.mnlbc.service;
 
 import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.DeserializationConfig;
@@ -12,35 +15,34 @@ import org.springframework.stereotype.Service;
 
 import com.oocl.mnlbc.dao.MealDAO;
 import com.oocl.mnlbc.dao.TrayDAO;
-import com.oocl.mnlbc.dao.UserDAO;
 import com.oocl.mnlbc.entity.Meal;
 import com.oocl.mnlbc.entity.Tray;
 
 @Service
 public class TrayService {
-	
+
 	final static Logger logger = Logger.getLogger(TrayService.class);
-	
+
 	@Autowired
 	TrayDAO trayDao;
-	
+
 	@Autowired
 	MealDAO mealDao;
-	
-	public List<Tray> getTrayByUserId(int userId){
+
+	public List<Tray> getTrayByUserId(int userId) {
 		logger.info("Getting all order by User ID");
 		List<Tray> trays = trayDao.getAllTrayByUserId(userId);
-		
+
 		for (Tray tray : trays) {
 			Meal meal = mealDao.find(tray.getMeal().getId());
 			tray.setMeal(meal);
 		}
-		
+
 		return trays;
 	}
-	
-	public boolean removeTrayByUserId(int userId){
-		List<Tray> trays =  getTrayByUserId(userId);
+
+	public boolean removeTrayByUserId(int userId) {
+		List<Tray> trays = getTrayByUserId(userId);
 		logger.info("Getting all order tray by User ID");
 		for (Tray tray : trays) {
 			trayDao.delete(tray);
@@ -48,8 +50,8 @@ public class TrayService {
 		logger.info("Removing all order by User ID");
 		return true;
 	}
-	
-	public boolean addTray(String trays){
+
+	public boolean addTray(String trays) {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(DeserializationConfig.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 		List<Tray> trayList = null;
@@ -66,6 +68,18 @@ public class TrayService {
 		logger.info("Adding  all order by User ID");
 		return true;
 	}
-	
-	
+
+	public String setTraySession(String trays, HttpSession session) {
+		session.setAttribute("trays", trays);
+		return "success";
+	}
+
+	public String getTraySession(HttpSession session) {
+		logger.info("Checking if session has logged in user");
+		String trays = (String) session.getAttribute("trays");
+		if (trays == null) {
+			trays = new String();
+		}
+		return trays;
+	}
 }
