b81659a18412281031bf2feaa1562fbbca4e4103
Mark Anthony S. Andes <mark.anthony.andes@oocl.com>
2016-07-29 17:50:17 +0800

    Project Folder Cleanup
    Andes,Mark 07/29/2016

---
+++ W7Project/UI/Publish/BurgerQueenClientPublish/resources/app/util/GlobalMessage.js
@@ -0,0 +1,16 @@
+Ext.define('com.oocl.mnlbc.GlobalMessage', {
+
+invalid_login : 'Invalid Username/Password',
+block_user : 'User is blocked',
+success_order : 'You have successfully entered your order! Your order will be delivered. Thank you for purchasing at Burger Queen',
+cancel_order : 'Order Canceled!',
+added_to_cart : 'Successfully Added to Tray!',
+existing_username : 'Username already exists!',
+existing_email : 'Email already exists',
+success_registration : 'Successfully Registered!',
+mismatched_password : 'Password mismatched!',
+safe_code_error : 'Safe Code Error',
+set_quantity : 'Please specify quantity',
+success_changePassword : 'Successfully Change Password',
+failed_toChangePassword : 'Incorrect Password'
+});
