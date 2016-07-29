09a953b0a33ca3e9611616758189969d8b9b6b2e
Aljun Dequilla <aljun.dequilla@gmail.com>
2016-07-29 15:20:22 +0800

    Added SQL Script insert data
    DEQUILLA

---
+++ W7Project/SQL/inquirydata.sql
@@ -0,0 +1,100 @@
+--------------------------------------------------------
+--  File created - Friday-July-29-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table INQUIRY
+--------------------------------------------------------
+
+  CREATE TABLE "GROUP1"."INQUIRY" 
+   (	"ID" NUMBER(15,0), 
+	"USER_ID" NUMBER(15,0), 
+	"MESSAGE" VARCHAR2(100 BYTE), 
+	"DATE_CREATED" DATE
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT" ;
+REM INSERTING into GROUP1.INQUIRY
+SET DEFINE OFF;
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (23,100031,'Nie Hau!',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (27,100031,'Yehey, love you guys',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (24,100031,'Hi love it',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (25,100031,'Hi again, love it',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (28,100031,'Hi, thanks for the discounts',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (1,100027,'Hello I am fan of your store . Can I ask if you have branch in this area. Thanks',to_date('08-MAR-12','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (3,100035,'Hi, we sell tomato ketchup. Contact 038729487 iif you want to order our ketchup for your burgers.',to_date('29-MAY-09','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (4,100028,'Does your burger use pork for your patties? Because I''m a muslim so... you know :P',to_date('13-APR-13','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (5,100043,'Poor treatment to customers',to_date('30-OCT-15','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (6,100047,'To whom it may concern, I''m allergic to peanuts.',to_date('01-SEP-13','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (7,100027,'Thanks for setting up your branch here. <3',to_date('27-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (21,100027,'badesa~',to_date('28-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (29,100031,'test',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (22,100031,'Hi! My name is Mark!',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (26,100027,'Hello from the otherside',to_date('29-JUL-16','DD-MON-RR'));
+Insert into GROUP1.INQUIRY (ID,USER_ID,MESSAGE,DATE_CREATED) values (30,100031,'Hi, Do you have a branch here in zhuhai',to_date('29-JUL-16','DD-MON-RR'));
+--------------------------------------------------------
+--  DDL for Index INQUIRY_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "GROUP1"."INQUIRY_PK" ON "GROUP1"."INQUIRY" ("ID") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT" ;
+--------------------------------------------------------
+--  Constraints for Table INQUIRY
+--------------------------------------------------------
+
+  ALTER TABLE "GROUP1"."INQUIRY" ADD CONSTRAINT "INQUIRY_PK" PRIMARY KEY ("ID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT"  ENABLE;
+  ALTER TABLE "GROUP1"."INQUIRY" MODIFY ("DATE_CREATED" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."INQUIRY" MODIFY ("MESSAGE" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."INQUIRY" MODIFY ("USER_ID" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."INQUIRY" MODIFY ("ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Ref Constraints for Table INQUIRY
+--------------------------------------------------------
+
+  ALTER TABLE "GROUP1"."INQUIRY" ADD CONSTRAINT "FK_USER_ID_INQUIRY" FOREIGN KEY ("USER_ID")
+	  REFERENCES "GROUP1"."USERS" ("ID") ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger INQUIRY_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "GROUP1"."INQUIRY_TRG" 
+BEFORE INSERT ON INQUIRY 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF INSERTING AND :NEW.ID IS NULL THEN
+      SELECT INQUIRY_SEQ.NEXTVAL INTO :NEW.ID FROM SYS.DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "GROUP1"."INQUIRY_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger INQUIRY_DATE
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "GROUP1"."INQUIRY_DATE" 
+BEFORE INSERT ON INQUIRY 
+FOR EACH ROW
+DECLARE
+  nowDate DATE;
+BEGIN
+
+  SELECT CURRENT_DATE INTO nowDate
+  FROM dual;
+
+  IF :NEW.DATE_CREATED IS NULL THEN
+    :NEW.DATE_CREATED := nowDate;
+  END IF;
+END;
+/
+ALTER TRIGGER "GROUP1"."INQUIRY_DATE" ENABLE;
---
+++ W7Project/SQL/mealdata.sql
@@ -0,0 +1,90 @@
+--------------------------------------------------------
+--  File created - Friday-July-29-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table MEAL
+--------------------------------------------------------
+
+  CREATE TABLE "GROUP1"."MEAL" 
+   (	"ID" NUMBER(15,0), 
+	"CODE" VARCHAR2(40 BYTE), 
+	"NAME" VARCHAR2(100 BYTE), 
+	"PRICE" FLOAT(126), 
+	"DESCRIPTION" VARCHAR2(200 BYTE), 
+	"IMAGE" VARCHAR2(200 BYTE), 
+	"CATEGORY" VARCHAR2(50 BYTE), 
+	"POINTS" NUMBER(*,0)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT" ;
+REM INSERTING into GROUP1.MEAL
+SET DEFINE OFF;
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100001,'M01','Fried Chicken',15,'Chicken! Chicken! Chicken! Taste our original recipe chicken made with 89 secret spices','resources/images/chicken/fried.png','Chickens',5);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100002,'M02','Coke',3,'Make your meal more delicious by adding one of the worlds famous sodas','resources/images/beverages/coke.png','Beverages',1);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100004,'M04','Coke Zero',3,'Coke Zero Fat Zero Grade!!! Just like your love ZERO.','resources/images/beverages/cokezero.png','Beverages',1);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100006,'M06','Sprite',3,'Sprite Tru White. ','resources/images/beverages/sprite.png','Beverages',1);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100007,'M07','Chicken Nuggets',5,'Chicken Nuggets! Nuggets for everwine! Taste the original chicken nuggets more precious than gold.','resources/images/chicken/Nuggets.png','Chickens',5);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100008,'M08','Bacon And Cheese',12,'What is better than bacon and cheese? BACON and CHEESE in a burger!','resources/images/burgers/baconAndCheese.png','Burgers',8);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100009,'M09','Onion Rings',6,'Rings? We got them! Try our special onion rings together with any dish that you want.','resources/images/sides/onionrings.jpg','Sides',2);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100012,'M12','XtraLong Cheeseburger',25,'Yes! Its extra long!','resources/images/burgers/XtraLongCheeseburger.png','Burgers',8);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100013,'M13','Caramel Sundae',5,'Caramel Sundae. Now with Camels and Sands','resources/images/desserts/caramelSundae.png','Desserts',3);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100014,'M14','Dr Pepper Shake',5,'Meet and Greet with Dr Pepper Shake','resources/images/desserts/drPepperShake.png','Desserts',3);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100015,'M15','Oreo Shake',5,'Oreo Shake. Never Oh Rio','resources/images/desserts/oreoShake.png','Desserts',3);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100016,'M16','Hash Brown',13,'Hash Brown,Number one favorite of HashTFive','resources/images/sides/hashbrown.png','Sides',2);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100094,'M66','Oreo Cheesecake',10,'OREO Ma Loveeee!','resources/images/Logo.png','desserts',2);
+Insert into GROUP1.MEAL (ID,CODE,NAME,PRICE,DESCRIPTION,IMAGE,CATEGORY,POINTS) values (100093,'M99','Royal',10,'Royal Milk Tea','resources/images/Logo.png','beverages',2);
+--------------------------------------------------------
+--  DDL for Index SYS_C007353
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "GROUP1"."SYS_C007353" ON "GROUP1"."MEAL" ("ID") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT" ;
+--------------------------------------------------------
+--  DDL for Index SYS_C007354
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "GROUP1"."SYS_C007354" ON "GROUP1"."MEAL" ("CODE") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT" ;
+--------------------------------------------------------
+--  Constraints for Table MEAL
+--------------------------------------------------------
+
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("POINTS" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("CATEGORY" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."MEAL" ADD CONSTRAINT "MEAL_CODE" UNIQUE ("CODE")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT"  ENABLE;
+  ALTER TABLE "GROUP1"."MEAL" ADD CONSTRAINT "PK_MEAL_ID" PRIMARY KEY ("ID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "WEEK5_PROJECT"  ENABLE;
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("DESCRIPTION" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("PRICE" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("NAME" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("CODE" NOT NULL ENABLE);
+  ALTER TABLE "GROUP1"."MEAL" MODIFY ("ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  DDL for Trigger MEAL_ID_TRIG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "GROUP1"."MEAL_ID_TRIG" 
+BEFORE INSERT ON MEAL
+FOR EACH ROW
+BEGIN
+IF :NEW.ID IS NULL THEN
+SELECT MEAL_ID_SEQ.nextval INTO :NEW.ID FROM SYS.DUAL;
+END IF;
+END;
+/
+ALTER TRIGGER "GROUP1"."MEAL_ID_TRIG" ENABLE;
