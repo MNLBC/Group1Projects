ead2d01ebcb3ef3c4c9b353630f703db3baabfa3
Ana Katrina De Leon <ana.katrina.de.leon@oocl.com>
2016-08-01 22:10:17 +0800

    Angular JS home work
    * Added trainee's contact no.

--- Angular/Ana Katrina De Leon/Angular JS Homework/app.js
+++ Angular/Ana Katrina De Leon/Angular JS Homework/app.js
@@ -6,37 +6,43 @@ $scope.myData = [
 id: 01,
 firstName:"Ana Katrina",
 lastName:"De Leon",
-email:"deleoan@oocl.com"
+email:"deleoan@oocl.com",
+contact:09151237719
 },
 {
 id: 02,
 firstName:"John Paul",
 lastName:"Limos",
-email:"limosjo@oocl.com"
+email:"limosjo@oocl.com",
+contact:09123456781
 },
 {
 id: 03,
 firstName:"Pauline Joyce",
 lastName:"Racelis",
-email:"racelpa@oocl.com"
+email:"racelpa@oocl.com",
+contact:09323656771
 },
 {
 id: 04,
 firstName:"Aljun",
 lastName:"Dequial",
-email:"dequial@oocl.com"
+email:"dequial@oocl.com",
+contact:09425348679
 },
 {
 id: 05,
 firstName:"Mark Anthony",
 lastName:"Andes",
-email:"andesma@oocl.com"
+email:"andesma@oocl.com",
+contact:09127356675
 },
 {
 id: 06,
 firstName:"Peter Emmanuel",
 lastName:"Barredo",
-email:"barrepe@oocl.com"
+email:"barrepe@oocl.com",
+contact:09215148779
 },
 
 ];
@@ -45,12 +51,13 @@ $scope.viewId = $routeParams.id;
 $scope.data = $scope.myData[$scope.viewId-1];
 $scope.onSearchBtnClick = function() {
         angular.forEach($scope.myData, function(value, key) {
-            if (value.firstName.indexOf( $scope.searchValue)>-1 || value.lastName.indexOf( $scope.searchValue)>-1 || value.email.indexOf( $scope.searchValue)>-1) {
+            if (value.firstName.indexOf( $scope.searchValue)>-1 || value.lastName.indexOf( $scope.searchValue)>-1) {
                 $scope.myData = [
                     {
                         firstName: value.firstName,
                         lastName: value.lastName,
-                        email: value.email
+                        email: value.email,
+                        contact: value.contact
                     }
                 ];
                 return myData;
